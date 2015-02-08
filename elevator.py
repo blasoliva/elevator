3a58d3a8ace14de7db72fefc3f87b1520323badc
Blas Oliva <blasoliva@gmail.com>
2015-02-07 21:21:52 -0300

    Create elevator.py

---
+++ elevator.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class elevator:
+
+    def __init__(self, uid, bottom, top):
+        """ Initialize variables
+        uid -- ID
+        top -- Top position
+        bottom -- Bottom position
+        position -- Current position
+        direction -- 0 : down / 1 : up 
+        queue_up -- Queue up requests
+        queue_down -- Queue down requests
+        clients -- Connected clients information
+        """ 
+        self.uid = uid
+        self.top = top
+        self.bottom = bottom
+        self.position = 0
+        self.direction = None
+        self.queue_up = []
+        self.queue_down = []
+        self.clients = []
+        
+    def goto(self, request):
+        """ Gets the request and moves up or down, set the current position
+        and returns the steps into a list to the main application
+        request - Requested position
+        position -- Current position
+        steps -- Motion's steps
+         """
+        steps = []
+
+        if request <= self.top \
+          and request >= self.bottom:
+            # Moving up
+            if request > self.position:
+                self.direction = 1
+                for i in range(request - self.position):
+                    steps.append("Moving up from %s to %s\n" % \
+                                    (i + self.position, request))
+            # Or moving down                        
+            elif request < self.position:
+                self.direction = 0
+                for i in range(self.position - request):
+                    steps.append("Moving down from %s to %s\n" % \
+                                    (self.position - i, request))
+            # Set current position
+            self.position = request
+            return steps
+            
+    def status(self):
+        """ Return current status """
+        return "\nID: %s \nTop: %s\nPosition: %s\nClients: %s\n" % \
+          (self.uid, self.top, self.position, len(self.clients)) + "\n"
+
+    def queue(self, request, direction):
+        """ Sorts the requests queue
+        Scans toward the nearest end and attends along the way.
+        Once hits the bottom or top it jumps to the other end and
+        moves in the same direction. Something like circular scan.
+        """
+        if direction is 1 and request not in self.queue_up:
+            self.queue_up.append(request)
+            self.queue_up.sort()
+            q = [ a for a in self.queue_up if a > self.current ] + \
+                [ b for b in self.queue_up if b < self.current ]
+            self.queue_up = q
+            
+        elif direction is 0 and request not in self.queue_down:
+            self.queue_down.append(request)
+            self.queue_down.sort(reverse=True)
+            q = [ a for a in self.queue_down if a < self.current ] + \
+                [ b for b in self.queue_down if b > self.current ]
+            self.queue_down = q
+
+    def catchclient(self, client):
+        """ Get connected clients information """
+        self.clients.append(client)
